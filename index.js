ad23788564c304a3ce2f7f89eeb8dbb8484b8c6d
thoufiq135 <shaikno150@gmail.com>
2025-11-08 15:11:09 +0530

    s

--- index.js
+++ index.js
@@ -1,8 +1,32 @@
 const express=require("express")
 const app=express()
 app.use(express.json())
+const slot1="green"
+const slot2="green"
+const slot3="green"
+const slot4="green"
 app.get("/",(req,res)=>{
     res.send("<h1>hello from server</h1>")
+})
+app.get("/get_data_esp",(req,res)=>{
+    const {s1,s2,s3,s4}= req.body
+    console.log(slot1)
+    console.log(slot2)
+    console.log(slot3)
+    console.log(slot4)
+    if(s1&&s2&&s3&&s4){
+        slot1=s1
+        slot2=s2
+        slot3=s3
+        slot4=s4
+        res.status(200).json({message:"got data😊"})
+    }else{
+        console.log(s1)
+        console.log(s2)
+        console.log(s3)
+        console.log(s4)
+        res.status(401).json({message:"no data😔"})
+    }
 })
 app.listen("3000",()=>{
     console.log("server is running on port 3000")
