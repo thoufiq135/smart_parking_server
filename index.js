1a3da055003deef66a8919aff13d6cf961086b7f
thoufiq135 <shaikno150@gmail.com>
2025-11-08 15:19:47 +0530

    j

--- index.js
+++ index.js
@@ -10,10 +10,10 @@ app.get("/",(req,res)=>{
 })
 app.post("/get_data_esp",(req,res)=>{
     const {s1,s2,s3,s4}= req.body
-    console.log(slot1)
-    console.log(slot2)
-    console.log(slot3)
-    console.log(slot4)
+    console.log(s1)
+    console.log(s2)
+    console.log(s3)
+    console.log(s4)
     if(s1&&s2&&s3&&s4){
         slot1=s1
         slot2=s2
