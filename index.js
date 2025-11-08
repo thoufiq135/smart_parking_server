94ecf268e008f9e3c2ffb24d1bfe5eb146c59b8e
thoufiq135 <shaikno150@gmail.com>
2025-11-08 17:50:24 +0530

    d

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 const express=require("express")
 const app=express()
 app.use(express.json())
-let slot1="yellow"
-let slot2="green"
+let slot1="green"
+let slot2="yellow"
 let slot3="green"
 let slot4="green"
 app.get("/",(req,res)=>{
     res.send("<h1>hello from server</h1>")
 })
+//esp main endpoints
 app.post("/get_data_esp",(req,res)=>{
     const {s1,s2,s3,s4}= req.body
     console.log(s1)
@@ -28,6 +29,14 @@ app.post("/get_data_esp",(req,res)=>{
         res.status(401).json({message:"no data😔"})
     }
 })
+app.get("/data_esp32",(req,res)=>{
+    if(slot1&&slot2&&slot3&&slot4){
+        res.status(200).json({s1:{slot1},s2:{slot2},s3:{slot3},s4:{slot4}})
+    }else{
+        res.status(400).json({message:"no data"})
+    }
+})
+//esp book end point
 app.get("/data_esp",(req,res)=>{
     if(slot1&&slot2&&slot3&&slot4){
         res.status(200).json({s1:{slot1},s2:{slot2},s3:{slot3},s4:{slot4}})
